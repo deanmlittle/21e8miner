d2cdf083203f082a305260993bb4f25e34bf22f1
= <=>
2020-02-29 15:50:29 +1100

    >=1

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function is21e8Out(script) {
     script.chunks[0].buf &&
     script.chunks[0].buf.length === 32 &&
     script.chunks[1].buf &&
-    script.chunks[1].buf.length > 1 &&
+    script.chunks[1].buf.length >= 1 &&
     script.chunks[2].opcodenum === Opcode.OP_SIZE &&
     script.chunks[3].opcodenum === Opcode.OP_4 &&
     script.chunks[4].opcodenum === Opcode.OP_PICK &&
